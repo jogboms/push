99faded8cf0621ee1f071f0b2a3f06431e2881be
jogboms <jaybladez09@gmail.com>
2016-02-19 17:30:52 +0100

    changes in example app

--- example/config/config.php
+++ example/config/config.php
@@ -30,7 +30,7 @@ return [
 		'use_debugger' => true,
 		
 		'app_dir' => '/App',
-		'templates_dir' => '/App/View',
+		'templates_dir' => '/App/Views',
 		'storage_dir' => '/storage',
 		'library_dir' => '/library',
 		'assets_dir' => '/www/assets',
